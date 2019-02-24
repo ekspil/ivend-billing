176946d442ab81282ad1dfb616d69f09f90da301
devnulled <devnulled@cryptaz.org>
2019-02-25 03:14:27 +0700

    app: add binding to 0.0.0.0 to acccess from outside container

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ Routes({fastify, knex, yandexKassaService})
 
 scheduler.scheduleTasks({knex, yandexKassaService})
 
-fastify.listen(3500, (err, address) => {
+fastify.listen(3500, "0.0.0.0", (err) => {
     if (err) throw err
 })
 
