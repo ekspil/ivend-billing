98e3d638cc92ebce9865bfd66843caf924cb947a
devnulled <devnulled@cryptaz.org>
2019-04-27 19:27:32 +0700

    ErrorHandler: add query logging for not found

--- app/routes/error/ErrorHandler.js
+++ app/routes/error/ErrorHandler.js
@@ -2,6 +2,7 @@ const ErrorHandler = (error, request, reply) => {
     console.error(error)
 
     if (error.message === "Not Found") {
+        console.log("Url not found " + request.req.url)
         return reply.type("application/json").code(404).send({message: "Not found"})
     }
 
