22a03090aed49ea40b559059782c886e0347a96c
devnulled <devnulled@cryptaz.org>
2019-04-27 20:03:12 +0700

    routes: add cast to number

--- app/routes/index.js
+++ app/routes/index.js
@@ -21,7 +21,7 @@ function Routes({fastify, knex, robokassaService}) {
             throw new Error("SignatureValidationError")
         }
 
-        const robopaymentStatus = await robokassaService.getPayment(InvId)
+        const robopaymentStatus = await robokassaService.getPayment(Number(InvId))
 
         console.log(robopaymentStatus)
 
