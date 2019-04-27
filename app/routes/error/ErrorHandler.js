5cd1ce71b94372e6bec9bc771a8661fe46591bec
devnulled <devnulled@cryptaz.org>
2019-04-27 19:54:25 +0700

    ErrorHandler: add cases

--- app/routes/error/ErrorHandler.js
+++ app/routes/error/ErrorHandler.js
@@ -6,6 +6,15 @@ const ErrorHandler = (error, request, reply) => {
         return reply.type("application/json").code(404).send({message: "Not found"})
     }
 
+    if (error.message === "SignatureValidationError") {
+        return reply.type("application/json").code(401).send({message: "Invalid signature"})
+    }
+
+    if (error.message === "RobokassaUnknownCode") {
+        console.error("Unknown response from Robokassa")
+        return reply.type("application/json").code(500).send({message: "Invalid signature"})
+    }
+
     return reply.type("application/json").code(500).send({message: "Internal Server Error"})
 }
 
