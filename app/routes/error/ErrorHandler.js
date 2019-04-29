ee87db34977ffa25eca8bf562ce4b0ed5c95e4b2
devnulled <devnulled@cryptaz.org>
2019-04-29 15:08:54 +0700

    ErrorHandler: improved logging

--- app/routes/error/ErrorHandler.js
+++ app/routes/error/ErrorHandler.js
@@ -1,20 +1,20 @@
 const ErrorHandler = (error, request, reply) => {
-    console.error(error)
-
     if (error.message === "Not Found") {
         console.log("Url not found " + request.req.url)
         return reply.type("application/json").code(404).send({message: "Not found"})
     }
 
     if (error.message === "SignatureValidationError") {
+        console.warn("Rejected request due invalid signature [401]")
         return reply.type("application/json").code(401).send({message: "Invalid signature"})
     }
 
     if (error.message === "RobokassaUnknownCode") {
         console.error("Unknown response from Robokassa")
-        return reply.type("application/json").code(500).send({message: "Invalid signature"})
+        return reply.type("application/json").code(500).send({message: "Internal Server Error"})
     }
 
+    console.error(error)
     return reply.type("application/json").code(500).send({message: "Internal Server Error"})
 }
 
