6a06a682e1ebae34b69856ec8f06fd3d4d2915b8
pshehmic <pshenmic@gmail.com>
2021-05-21 13:59:31 +0700

    add port

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const knex = require("knex")({
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
+        port: process.env.POSTGRES_PORT,
         ssl: true
     }
 })
